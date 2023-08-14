1c1e498f691f3e61b7e004381e9bddb8f81036e0
dennis <dennisndungumwangi@gmail.com>
2023-08-14 14:12:28 -0700

    C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints all single digit numbers of base 10 starting from 0
+ *
+ * Return: Always (Success)
+ */
+
+int main(void)
+{char c;
+
+for (c = '0'; c <= '10'; c++)
+{
+{
+putchar(c);
+}
+}
+
+putchar('\n');
+
+return (0);
+}
