a92337039c6c50bfa15bc4ac02280843ed24da6d
dennis <dennisndungumwangi@gmail.com>
2023-08-14 14:50:18 -0700

    C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints all single digit numbers of
+ * base 16 starting from 0
+ *
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+char c;
+
+for (c = '0'; c <= '15'; c++)
+{
+{
+putchar(c);
+}
+}
+
+putchar('\n');
+
+return (0);
