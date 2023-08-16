2ab03565713a8e3f6844700c5494aa2ba7f6884e
dennis <dennisndungumwangi@gmail.com>
2023-08-16 05:33:37 -0700

    C - Functions, nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <main.h>
+#include "main.h"
 
 /**
  * main - prints _putchar
@@ -10,7 +10,7 @@
  * return : always return 0(Success)
  *
  */
-int main(void)
+int print_putchar(void)
 {
 
 putchar('_');
