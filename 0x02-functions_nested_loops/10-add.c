b8b027e72d98225c05ac248edbc558389e80ccd1
dennis <dennisndungumwangi@gmail.com>
2023-08-17 16:48:39 -0700

    C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - Entry point
+ * Description: function that adds two integers and returns the result.
+ * @a: first integer
+ * @b: second interger
+ * Return: Always 0.
+ */
+
+int add(int a, int b)
+{
+
+	return (a + b);
+}
