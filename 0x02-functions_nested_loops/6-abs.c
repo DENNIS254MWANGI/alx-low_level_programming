a7d20869b4fdc33207ae79fec0ac2d0749251b13
dennis <dennisndungumwangi@gmail.com>
2023-08-17 07:06:20 -0700

    C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _abs - Entry point
+ * Description: function that computes the absolute value of an integer.
+ * @a: character to be checked
+ * Return: the absolute value of an integer.
+ */
+
+int _abs(int a)
+{
+
+	if (a >= 0)
+	{
+		return (a);
+
+	else
+	{
+		return (-a);
+	}
+}
