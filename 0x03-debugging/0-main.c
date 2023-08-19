0cdf57fdc5b6028ac7146d2824deb04b7ed20efd
dennis <dennisndungumwangi@gmail.com>
2023-08-19 02:33:00 -0700

    C - Debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * positive_or_negative - Checks if number is positive, zero or negative
+ * @a: gives the correct output when given a case of 0.
+ *
+ * Return: Always (Success)
+ */
+void positive_or_negative(int a)
+{
+	int a;
+
+	a = 0;
+
+	positive_or_negative(a);
+	return (0);
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,5 @@
+#ifndef HOLBERTON_H_INCLUDED
+#define HOLBERTON_H_INCLUDED
+
+int _putchar(char c);
+void postitive_or_negative(int a);
