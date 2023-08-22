5ed0affb10e7fd690068a8ab544656bf4171e032
dennis <dennisndungumwangi@gmail.com>
2023-08-22 03:32:45 -0700

    C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -13,13 +13,16 @@ void print_rev(char *s)
 {
 	int a;
 
-	for (a = 0; s[a] != '\0'; a++)
-	{
+	a = 0;
 
-		for (a -= 1; a >= 0; a--)
-		{
-			_putchar(s[a]);
-		}
+	while (s[a] != '\0';)
+	{
+		a++
+	}
+
+	for (a -= 1; a >= 0; a--)
+	{
+		_putchar(s[a]);
 	}
 
 	_putchar('\n');
