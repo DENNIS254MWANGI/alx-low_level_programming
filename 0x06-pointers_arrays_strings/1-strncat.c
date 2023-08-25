3766f28df6dd0d03e76e0e664856487f6fb361a3
dennis <dennisndungumwangi@gmail.com>
2023-08-25 03:54:40 -0700

    C - More pointers, arrays and strings C

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * *_strcat - function that concatenates two strings
+ * @dest:appends destination string
+ * @src: appends source string
+ * @n: number of bytes
+ *
+ * Return: pointer to the resulting string dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int a = 0, b = 0;
+
+	while (dest[a] != '\0')
+	{
+		a++;
+	}
+	while (src[b] != '\0' && b < n)
+	{
+		dest[a + b] = src[b];
+		b++;
+	}
+	dest[a + b] = '\0';
+
+	return (dest);
+}
