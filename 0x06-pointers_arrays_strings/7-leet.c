e674312d9453e70a1ea133fe448abd33ad6234c2
dennis <dennisndungumwangi@gmail.com>
2023-08-26 15:35:44 -0700

    C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -13,31 +13,16 @@ char *leet(char *str)
 	for (a = 0; str[a] != '\0'; a++)
 	{
 		while (str[a] == 'a' || str[a] == 'A')
-		{
 			str[a] = '4';
-		}
-
 		while (str[a] == 'e' || str[a] == 'E')
-		{
 			str[a] = '3';
-		}
-
 		while (str[a] == 'o' || str[a] == 'O')
-		{
 			str[a] = '0';
-		}
-
 		while (str[a] == 't' || str[a] == 'T')
-		{
 			str[a] = '7';
-		}
-
 		while (str[a] == 'l' || str[a] == 'L')
-		{
 			str[a] = '1';
-		}
 	}
 
 	return (str);
 }
-
