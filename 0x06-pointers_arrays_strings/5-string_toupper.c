b080874a03f4e339da91bf8ae1d5a2f9bba477fc
dennis <dennisndungumwangi@gmail.com>
2023-08-26 01:43:56 -0700

    C - More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * string_toupper - reverse the contents of an array of integers
+ *@str: string
+ *Return: return always successful
+ */
+
+char *string_toupper(char *str)
+{
+	int a = 0;
+
+	while (str[a] = '\0')
+	{
+
+		str[a] = toupper(str[a]);
+
+		a++;
+	}
+}
