d1d1e0f6baffe2dc0095ac223e2182a8048508e8
dennis <dennisndungumwangi@gmail.com>
2023-08-29 12:06:10 -0700

    C - Even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _memcpy - function that copies memory area.
+ * @dest: pointer destination
+ * @src: pointer source
+ * @n: constant
+ * Return: return always successful
+ *
+ */
+
+unsigned int _strspn(char *s, char *accept)
