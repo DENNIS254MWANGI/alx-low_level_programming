494466d5f654da888f4ea79780e3c0250c8175de
dennis <dennisndungumwangi@gmail.com>
2023-08-29 13:31:52 -0700

    C - Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,12 +1,37 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _memcpy - function that copies memory area.
- * @dest: pointer destination
- * @src: pointer source
- * @n: constant
+ * _strspn - function that gets the length of a prefix substring.
+ * @s: pointer destination
+ * @accept: pointer source
  * Return: return always successful
  *
  */
 
 unsigned int _strspn(char *s, char *accept)
+{
+	int a, b;
+
+	a = 0;
+	b = 0;
+	int c = 0;
+
+	while (s[a] != '\0')
+	{
+		if (s[a] != 32)
+		{
+			while (accept[b] != '\0')
+			{
+				if (s[a] == accept[b])
+					c++;
+				b++;
+			}
+		a++;
+		}
+		else
+			return (c);
+
+	}
+	return (c);
+}
