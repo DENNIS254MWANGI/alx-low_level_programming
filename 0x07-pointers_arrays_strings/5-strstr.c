5d2ef5b2fcc83a6029d13d42b9e97d90f10e731c
dennis <dennisndungumwangi@gmail.com>
2023-08-30 09:48:52 -0700

    C - Even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strstr - check the code
+ *@haystack: pointer string
+ *@needle: pointer substring
+ * Return: Always 0.
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int a, b;
+
+	a = 0;
+	b = 0;
+
+	while (needle[b] != '\0')
+		s++;
+
+	while (*haystack)
+	{
+		while (needle[a])
+		{
+			if (haystack[a] != needle[a])
+				a++;
+				break;
+		}
+		if (a != b)
+			haystack++;
+		else
+			return (haystack);
+	}
+	return (NULL);
+}
