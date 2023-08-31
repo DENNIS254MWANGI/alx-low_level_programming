0635454e449f0b9ba0ddf188598b61948695af1b
dennis <dennisndungumwangi@gmail.com>
2023-08-31 13:12:33 -0700

    C - Recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdlib.h>
 /**
  * _puts_recursion - function that
  * prints a string, followed by a new line.
---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _sqrt_recursion - function that returns the
+ * natural square root of a number.
+ * @n: constant
+ * Return: Always 0.
+ */
+
+int _sqrt_recursion(int n)
+{
+
