b25abb5c15a76dd4d13d7ea308f881dc70e7a95a
dennis <dennisndungumwangi@gmail.com>
2023-09-01 04:48:46 -0700

    C Recursion.

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,6 +2,23 @@
 #include <stdio.h>
 
 /**
+ * get_sqrt - function that returns the
+ * natural square root of a number.
+ * @a: number to power
+ * @z: number to be powered
+ * Return: Always 0.
+ */
+
+int get_sqrt(int a, int z)
+int _sqrt_recursion(int n)
+{
+	if (a * a == z)
+		return (n);
+	if (n * n > z)
+		return (-1);
+	return (get_sqrt(n + 1, z));
+}
+/**
  * _sqrt_recursion - function that returns the
  * natural square root of a number.
  * @n: constant
@@ -10,4 +27,5 @@
 
 int _sqrt_recursion(int n)
 {
-
+	return (get_sqrt(1, n));
+}
