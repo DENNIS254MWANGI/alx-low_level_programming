aeec2cb7a03d26e755ac04709053b298a048b1b8
dennis <dennisndungumwangi@gmail.com>
2023-09-06 08:45:50 -0700

    C - malloc, free

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
+
+char *argstostr(int ac, char **av)
+{
+	char *z, *rep;
+	int a, b, all;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (a = 0, all = 0; a < ac; a++)
+	{
+		for (b = 0; *(*(av + a) + b) != '\0'; b++, all++)
+			;
+		all++;
+	}
+	all++;
+
+	z = malloc(all * sizeof(char));
+	if (z == NULL)
+		return (NULL);
+
+	rep = z;
+	for (a = 0; a < ac; a++)
+	{
+		for (b = 0; av[a][b] != '\0'; b++)
+		{
+			*z = av[a][b];
+			z++;
+		}
+		*z = '\n';
+		z++;
+	}
+
+	return (rep);
+}
