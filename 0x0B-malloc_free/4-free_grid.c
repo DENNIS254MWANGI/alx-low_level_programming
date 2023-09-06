d31d4711ca7e12be58bc7452326cca743e8ca4e5
dennis <dennisndungumwangi@gmail.com>
2023-09-06 07:45:53 -0700

    C - malloc, free

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -6,7 +6,6 @@
  * free_grid - function that frees a 2 dimensional grid
  * previously created by your alloc_grid function.
  * @grid: the address of the two dimensional grid
- * @width: width of the grid
  * @height: height of the grid
  *
  * Return: Nothing.
